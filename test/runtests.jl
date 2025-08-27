535758fdb395600b464e494f6dc294c0c4b18c55
Dongdong Kong <kongdd@users.noreply.github.com>
2025-08-27 19:12:46 +0800

    fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,9 @@
 using SpatInterp, Test
 
 include("test-bilinear_irregular.jl")
-include("test-interp_bilinear.jl")
 include("test-interp.jl")
+include("test-interp_bilinear.jl")
+include("test-interp_tps.jl")
 
 include("test-angle.jl")
 include("test-find_neighbor.jl")
